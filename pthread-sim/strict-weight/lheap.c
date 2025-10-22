bd81f3d687cade66be85bc6a41ec0c71028213b4
Frans Kaashoek <kaashoek@mit.edu>
2025-10-22 00:09:19 -0400

    oops

---
+++ pthread-sim/strict-weight/lheap.c
@@ -0,0 +1,45 @@
+#include <pthread.h>
+#include <stdlib.h>
+
+#include "util.h"
+#include "lheap.h"
+
+struct lock_heap *lh_new(int grp_cmp(void *, void*)) {
+	struct lock_heap *lh = (struct lock_heap *) malloc(sizeof(struct lock_heap));
+	lh->heap = heap_new(grp_cmp);
+	lh->wait_for_wr_heap_lock_cycles = 0;
+	lh->num_times_wr_heap_locked = 0;
+	atomic_init(&lh->wait_for_rd_heap_lock_cycles, 0);
+	atomic_init(&lh->num_times_rd_heap_locked, 0);
+	pthread_rwlock_init(&lh->heap_lock, NULL);
+	return lh;
+}
+
+void lh_unlock(struct lock_heap *lh) {
+	pthread_rwlock_unlock(&lh->heap_lock);
+}
+
+void lh_lock(struct lock_heap *lh) {
+	pthread_rwlock_wrlock(&lh->heap_lock);
+}
+
+void lh_rdlock(struct lock_heap *lh) {
+	pthread_rwlock_rdlock(&lh->heap_lock);
+}
+
+// Wrapper functions for pthread_rwlock operations with timing
+void lh_lock_timed(struct lock_heap *lh) {
+	int start_tsc = safe_read_tsc();
+	lh_lock(lh);
+	int end_tsc = safe_read_tsc();
+	lh->wait_for_wr_heap_lock_cycles += (end_tsc - start_tsc);
+	lh->num_times_wr_heap_locked++;
+}
+
+void lh_rdlock_timed(struct lock_heap *lh) {
+	int start_tsc = safe_read_tsc();
+	lh_rdlock(lh);
+	int end_tsc = safe_read_tsc();
+	atomic_fetch_add_explicit(&lh->wait_for_rd_heap_lock_cycles, (end_tsc - start_tsc), memory_order_relaxed);
+	atomic_fetch_add_explicit(&lh->num_times_rd_heap_locked, 1, memory_order_relaxed);
+}

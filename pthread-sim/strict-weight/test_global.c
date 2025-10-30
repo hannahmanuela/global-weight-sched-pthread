b06c752e6cacdaf8de6e511439f60669fbc73336
Frans Kaashoek <kaashoek@mit.edu>
2025-10-30 09:27:00 -0400

    Move worst test into test_global.c

--- pthread-sim/strict-weight/test_global.c
+++ pthread-sim/strict-weight/test_global.c
@@ -77,8 +77,37 @@ void test_mheap(int nheap) {
 	printf("test_%d_mheap ok\n", nheap);
 }
 
+void test_mheap_many(int nheap) {
+}
+
+void test_worst(int nheap) {
+	int n = 10000;
+	int sum = 0;
+	int seed = getpid();
+	int worst;
+	for(int t = 0; t < n; t++) {
+		struct mheap *mh = mh_new(grp_cmp, nheap, seed+t);
+		struct group *g = grp_new(0, 10);
+		mh_add_group(mh, g);
+
+		struct process *p = grp_new_process(1, g);
+		enqueue(p);
+
+		for (int i = 0; ; i++) {
+			if ((p = schedule(0, mh)) != NULL) {
+				sum += i;
+				if(i > worst)
+					worst = i;
+				break;
+			}
+		}
+	}
+	printf("test_worst: avg %d worst %d\n", sum/n, worst);
+}
+
 void main(int argc, char *argv[]) {
 	test_mheap(1);
 	test_mheap(2);
+	test_worst(112);
 }
 

2bbfbeeca79adbe25593803257ba4720cef4e3a1
Frans Kaashoek <kaashoek@mit.edu>
2025-10-31 19:35:08 -0400

    yield with random run time

--- pthread-sim/strict-weight/test_global.c
+++ pthread-sim/strict-weight/test_global.c
@@ -80,8 +80,8 @@ void test_mheap(int nheap) {
 	printf("-- test_%d_mheap ok\n", nheap);
 }
 
-void test_mheap_many_grp(int nheap) {
-	printf("== test_%d_mheap grp %d\n", nheap, GRP10); 
+void test_mheap_many_grp(int nheap, bool rand) {
+	printf("== test_%d_mheap grp %d %d\n", nheap, rand, GRP10); 
 	int n = 100000;
 	int tl = 4000;
 	struct group *gs[GRP10];
@@ -94,15 +94,19 @@ void test_mheap_many_grp(int nheap) {
 	struct mheap *mh = mk_mheap(nheap, GRP10, PROC2, tl, gs, ws);
 	for (int i = 0; i < n; i++) {
 		struct process *p = schedule_retry(0, mh);
-		yield(p, mh->tick_length);
-		ticks[p->group->group_id] += mh->tick_length;
+		int tl = mh->tick_length;
+		if(rand) {
+			tl = random() % mh->tick_length;
+		}
+		yield(p, tl);
+		ticks[p->group->group_id] += tl;
 	}	
 	for (int i = 1; i < GRP10; i++) {
 		float w = (1.0 * ticks[i])/ticks[0];
-		float m = 0.1;
+		float m = 0.12;
 		float l = (i+1)-m;
 		float h = (i+1)+m; 
-		// printf("%0.2f %0.2f %0.2f\n", w, l, h);
+		// printf("ticks %0.2f l %0.2f h %0.2f\n", w, l, h);
 		assert(w >= l && w <= h);
 	}
 	printf("-- test_%d_mheap grp %d: OK\n", nheap, GRP10); 
@@ -202,9 +206,11 @@ void test_worst(int nheap) {
 void main(int argc, char *argv[]) {
 	test_mheap(1);
 	test_mheap(2);
-	test_mheap_many_grp(1);
-	test_mheap_many_grp(2);
-	test_mheap_many_grp(5);
+	test_mheap_many_grp(1, 0);
+	test_mheap_many_grp(2, 0);
+	test_mheap_many_grp(5, 0);
+	test_mheap_many_grp(1, 1);
+	test_mheap_many_grp(5, 1);
 	test_mheap_sleep(1, 0, GRP2);
 	test_mheap_sleep(1, 1, GRP2);
 	test_mheap_sleep(1, 2, 3);

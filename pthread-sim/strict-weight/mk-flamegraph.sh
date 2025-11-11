a2247edf1942ec3524903bc784df31acd56054b6
Frans Kaashoek <kaashoek@mit.edu>
2025-11-11 12:53:45 -0500

    x

--- pthread-sim/strict-weight/mk-flamegraph.sh
+++ pthread-sim/strict-weight/mk-flamegraph.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 perf script | stackcollapse-perf.pl > out.perf-folded
-flamegraph.pl out.perf-folded > flamegraph.svg
-xdg-open flamegraph.svg &
+flamegraph.pl out.perf-folded > $1.svg
+xdg-open $1.svg &

7a30e7d47e15c6f090ba5983d290c8e00b5a140d
Frans Kaashoek <kaashoek@mit.edu>
2025-11-11 15:28:55 -0500

    x

--- pthread-sim/strict-weight/mheap.h
+++ pthread-sim/strict-weight/mheap.h
@@ -19,7 +19,6 @@ void mh_free(struct mheap *mh);
 int mh_empty(struct group *g);
 void mh_print(struct mheap *mh);
 vt_t mh_min_vt(struct lheap *lh);
-struct lheap *mh_heap(struct mheap *, int i);
 struct process *mh_min_proc(struct core *c, struct mheap *mh);
 struct lheap *mh_choose_heap(struct core *c, struct mheap *mh);
 void mh_add_process(struct core *c, struct process *p, struct lheap *lh);

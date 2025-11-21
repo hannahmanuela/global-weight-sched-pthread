647559fccf974222423e5acd41f5b4fb60f8f1d0
Frans Kaashoek <kaashoek@mit.edu>
2025-11-20 20:00:37 -0500

    x

--- pthread-sim/strict-weight/vt.h
+++ pthread-sim/strict-weight/vt.h
@@ -9,7 +9,6 @@ typedef int w_t;
 // XXX handle vt_t wrap around
 #define DUMMY (INT_MAX)   // if vt_t is DUMMY, then dummy heap_elem
 
-// delta_exec * standard_weight / actual_weight
 static vt_t __calc_delta(vt_t delta_exec, w_t standard_weight, w_t actual_weight)
 {
         return delta_exec * standard_weight / actual_weight;
@@ -17,9 +16,6 @@ static vt_t __calc_delta(vt_t delta_exec, w_t standard_weight, w_t actual_weight
 
 static inline vt_t calc_delta(vt_t delta, w_t weight)
 {
-        // if (weight != EXP_WEIGHT)
-        //      delta = __calc_delta(delta, EXP_WEIGHT, weight);
-        //printf("delta: %d, weight: %d, %d\n", delta, weight, delta/weight);
         delta = delta / weight;
         return delta;
 }

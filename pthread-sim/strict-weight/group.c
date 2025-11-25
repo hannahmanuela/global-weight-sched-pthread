75e419456a4102078bb0e4640315d585dd418fe0
Frans Kaashoek <kaashoek@mit.edu>
2025-11-25 11:45:26 -0500

    x

--- pthread-sim/strict-weight/group.c
+++ pthread-sim/strict-weight/group.c
@@ -73,7 +73,7 @@ static float grp_runtime(struct group *g, long t) {
 	for (struct process *p = g->procs; p != NULL; p = p->next) {
 		run += (float)(p->runtime);
 	}
-	printf("  = %d: grp runtime %f fraction %0.2f:\n    ", g->gid, run, AVG(run, t));
+	printf("  = %d: w %d grp runtime %f fraction %0.2f:\n    ", g->gid, g->weight, run, AVG(run, t));
 	for (struct process *p = g->procs; p != NULL; p = p->next) {
 		printf("[%d: %ld %0.2f] ", p->pid, p->runtime, AVG(p->runtime, run));
 	}

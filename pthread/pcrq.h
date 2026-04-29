94d5c18b98d82ad41eec515cd936edf6f27eb7d3
Frans Kaashoek <kaashoek@mit.edu>
2026-04-29 19:36:57 -0400

    Start on per-core runqueue scheduler

---
+++ pthread/pcrq.h
@@ -0,0 +1,7 @@
+#include "core.h"
+#include "global_heap.h"
+
+bool gh_schedule_pcrq(struct global_heap *gh, struct core *c);
+void gh_yield_pcrq(struct global_heap *gh, struct core *c, struct process *p, t_t time_passed);
+void gh_enqueue_pcrq(struct global_heap *gh, struct core *c, struct process *p);
+void gh_dequeue_pcrq(struct global_heap *gh, struct core *c, struct process *p, t_t time_gotten);

c17197e614d1fea63c4e29294ee4d2b0c9f9ccf0
Frans Kaashoek <kaashoek@mit.edu>
2026-04-29 14:57:35 -0400

    x

--- pthread/rankerror.sh
+++ pthread/rankerror.sh
@@ -5,10 +5,9 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-# ./global-heap -2 -s -l /tmp/$3 -t 2 $1 $2
-
-#./global-heap -2 -s -b 1 -t 2 2 4
-./global-heap -2 -s -t 2 2 4
+./global-heap -s -l /tmp/$3 -t 2 $1 $2
+#./global-heap -s -b 1 -t 2 2 4
+#./global-heap -s -t 2 2 4
 
 ./logmerge $1 $3
 ./rankerror $3

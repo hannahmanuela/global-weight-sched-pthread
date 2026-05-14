34abc14552e3f76777adf84aec125440f4050ca7
Frans Kaashoek <kaashoek@mit.edu>
2026-05-13 20:31:07 -0400

    tp measurement script for a given number of cores

---
+++ pthread/run-tp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCHEDULERS=("rr" "gwfs" "gq" "pcrq")
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <ncore>"
+    exit 1
+fi
+
+echo "----low load"
+
+for s in ${SCHEDULERS[@]}; do
+    ./schedule $s $1 $1
+done
+
+echo "----high load"
+
+for s in ${SCHEDULERS[@]}; do
+    ./schedule $s $1 $(($1 * 4))
+done

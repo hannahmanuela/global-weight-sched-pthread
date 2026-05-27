e48a01e9919f3a6f268056d1e36221b56ab59808
Frans Kaashoek <kaashoek@mit.edu>
2026-05-27 07:39:14 -0400

    x

---
+++ pthread/test-rr.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# interesting scenarios for rr
+
+echo "==== scale under high load"
+./schedule rr 4 64
+
+echo "==== scalability under low load"
+./schedule rr 4 4
+
+echo "==== sleeping process 0 will run after all other runnable processes"
+./schedule -b 1 rr 4 64
+
+echo "==== two priority levels, hitting the preempt path"
+./schedule -p -b 2 -g 2 rr 4 6

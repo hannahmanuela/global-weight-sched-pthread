54e43c5c23028c929ef2674440a751ffec739450
Frans Kaashoek <kaashoek@mit.edu>
2026-06-16 08:48:49 -0400

    x

---
+++ pthread/run-exp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# NUM_CORES=(1 2 4 8 16 24 32 40 48)
+NUM_CORES=(1 2 4)
+SCHEDULERS=("rr" "gwfs" "gq" "pcrq")
+D=exp-out-`date +%Y-%m-%d_%H-%M-%S`
+
+mkdir $D
+echo $D
+
+for s in ${SCHEDULERS[@]}; do
+    echo $s
+    for n in ${NUM_CORES[@]}; do
+	./schedule -y $s $n $(($n * 4))
+    done 2>&1 > $D/tp-$s.out
+    grep tp $D/tp-$s.out | awk '{print $2, $3}' > $D/tp-$s.dat
+done
+
+echo "mheap"
+
+for n in ${NUM_CORES[@]}; do
+	./test-mheap $n
+done 2>&1 > $D/tp-mheap.out
+grep tp $D/tp-mheap.out | awk '{print $2, $3}' > $D/tp-mheap.dat
+
+echo "rr w priority and preemption"
+
+for n in ${NUM_CORES[@]}; do
+    p=$(($n - 1))
+    ./schedule -p -q -b 2 -g 2 rr $n $(($p * 2))
+done 2>&1 > $D/tp-rr-prio.out
+grep tp $D/tp-rr-prio.out | awk '{print $2, $3}' > $D/tp-rr-prio.dat

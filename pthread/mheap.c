f957a71deeb01b7c2df4478e9f74c15e2e407abf
Frans Kaashoek <kaashoek@mit.edu>
2026-07-01 12:46:44 -0400

    oops

--- pthread/mheap.c
+++ pthread/mheap.c
@@ -351,8 +351,9 @@ struct heap_elem *mh_deq_min_elem_all_heap(struct mheap *mh, is_min_elem_t is_mi
 	int s = mycore()->cid;
 	for (int i = 0; i < mh->nheap; i++) {
 		struct heap *h = mh->h[MH_IND(mh, s+i)];
-		struct heap_elem *he = heap_min(h);
-		if (is_min_elem(he) && mh_try_del_min(h, he)) {
+		struct heap_elem *he0 = heap_min(h);
+		if (is_min_elem(he0) && mh_try_del_min(h, he0)) {
+			he = he0;
 			he->tsc_out = safe_read_tsc();
 			lock_release(&h->lk);
 			break;
